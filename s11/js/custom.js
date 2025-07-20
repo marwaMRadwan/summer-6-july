d8038a0d2f7d18d9d238834649caa64316fe16ff
marwaMRadwan <marwaMRadwan>
2025-07-20 14:15:23 +0300

    s11

---
+++ s11/js/custom.js
@@ -0,0 +1,155 @@
+// console.log("test file")
+
+// variable
+//any
+// var let const
+// var x= 10
+// var x = "marwa"
+// var x = 15
+// console.log(x)
+// if(true){
+//     var x  = 9
+// }
+
+// console.log(x)
+
+// let x = 5
+// x = 3
+
+// if(true){
+//     let x  = 9
+// }
+
+// console.log(x)
+
+// const PI = 3.14
+
+
+// operators
+// math => + - / * ** %
+// assignment. = += -= *= /= ....
+//increament and decrement  unary  ++ --
+// comporisan > < >= <= != !== == === 
+// terny operator ?
+// logical operator && || !
+
+// let convert = 35 , convertType = "cTOf", result  // cTOf or fToc
+
+// convertType == 'fTOc' ? result=  9 / 5  * convert + 32 : result = 5 / 9 * (convert - 32)
+
+// console.log(result)
+
+// convert += 7 //  convert = convert + 7
+
+// let x = 3
+// console.log(++x)
+// console.log(x)
+
+//boolean true false
+// let x = 5 , y ="5"
+// console.log(x===y)
+
+
+// let x = true
+// console.log( typeof x )
+
+// comparison statement
+// if else
+//     let data = 9
+// if( data >= 10 )
+//     console.log("test")
+// else
+//     console.log("vgsvs")
+
+// let name= "marwa"
+// if(name == "mohammed"){
+
+// }
+// else if(name == "marwa"){
+    
+// }
+// else if(name == "ahmed"){
+
+// }
+
+
+// let scroe = 75, grade
+
+// if( scroe >= 90) grade = "A"
+// else if( scroe >= 80) grade = "B"
+// else if( scroe >= 70) grade = "C"
+// else if( scroe >= 60) grade = "D"
+// else grade = "F"
+// console.log(grade)
+
+// switch
+// let monthNumber = 15, mon
+// switch(monthNumber){
+//     case 1:
+//         mon = "jan"
+//         console.log(mon)
+//         break
+//     case 2:
+//         mon = "feb"
+//         console.log(mon)
+//         break
+//     case 3:
+//         mon = "mar"
+//         console.log(mon)
+//         break
+//     case 4:
+//         mon = "apr"
+//         console.log(mon)
+//         break
+//     case 5:
+//         mon = "may"
+//         console.log(mon)
+//         break
+//     case 6:
+//         mon = "jun"
+//         console.log(mon)
+//         break
+//     case 7:
+//         mon = "jul"
+//         console.log(mon)
+//         break
+//     case 8:
+//         mon = "aug"
+//         console.log(mon)
+//         break
+//     case 9:
+//         mon = "sep"
+//         console.log(mon)
+//         break
+//     case 10:
+//         mon = "oct"
+//         console.log(mon)
+//         break
+//     case 11:
+//         mon = "nov"
+//         console.log(mon)
+//         break
+//     case 12:
+//         mon = "dec"
+//         console.log(mon)
+//         break
+//     default:
+//         console.log("invalid month number")
+// }
+
+
+// truthy and false value
+
+// 0  -0  0n  ""  null  undefined NAN false
+
+// any number or string true [] {} function
+// let value = ""
+// console.log(Boolean(value))
+
+// let num = "5"
+// num = Number(num)
+
+
+// console.log(x);
+
+// let x = 3
